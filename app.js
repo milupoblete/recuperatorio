2e395a9b8c2c934324b37ecab656f605ccb841f6
milupoblete <136114129+milupoblete@users.noreply.github.com>
2023-09-27 21:56:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".mySwiper-1",{
+    sliderPerView: 1,
+    spaceBetween: 30,
+    loop:true,
+    pagination:{
+        el:".swiper-pagination",
+        clickable: true,
+    },
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+
+});
+
+var swiper = new Swiper(".myswiper-2",{
+    sliderPerView: 3,
+    spaceBetween: 20,
+    loop:true,
+    loopFillGroupWithBlank: true,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    },
+    brekpoints: {
+        0: {
+            sliderPerView:1,
+        },
+        520: {
+            sliderPerView:2,
+        },
+        950:{
+            sliderPerView:3,
+        },
+    }
+})
+
+let tabInputs = ducument.querySelectorAll(".tabInput");
+tabInputs.forEach(function(input){
+
+    input.addEventListener(`change`, function(){
+        let id = input.ariaValueMax;
+        let thisSwiper = document.getElementById(`swiper` + id);
+        thisSwiper.swiper.update()
+    })
+});
+
